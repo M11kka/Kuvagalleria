63f67398c3b5bdb19756fdeb0b931c5fc7068c6b
M11kka <miikka@Matkyla.local>
2017-05-02 12:47:56 +0300

    commit

--- kuvagalleria.php
+++ kuvagalleria.php
@@ -9,8 +9,10 @@
 <body>
     <form action="tallenna.php" enctype="multipart/form-data" method="POST">
         <input type="hidden" name="MAX_FILE_SIZE" value="1000000" />
-        <input type="file" name="kuva" />
-        <input type="submit" name="submit" value="Upload" />
+        <input type="file" name="kuva" ><br>
+        <p>Kuvateksti:<input type="text" name="kuvateksti" ></p>
+        <input type="submit" name="submit" value="Upload" /><br>
     </form>
 </body>
 </html>
+
--- tallenna.php
+++ tallenna.php
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-<?php    
+<?php     
 if(isset($_POST['submit'])) {
 
 // process the form data
@@ -15,16 +15,27 @@ if(isset($_POST['submit'])) {
 $tmp_file = $_FILES['kuva']['tmp_name'];
 $target_file = basename($_FILES['kuva']['name']);
 $upload_dir = "uploads";
+$kuvateksti = $_POST['kuvateksti'];
 
 // move_uploaded_file will return false if $tmp_file is
 // not a valid upload file or if it cannot be moved for
 // any other reason
 
-if(move_uploaded_file($tmp_file, $upload_dir."/".$target_file)){
-echo "File uploaded successfully.";
-} else {
-    echo $_FILES['kuva']['error'];
-}
+ if(move_uploaded_file($tmp_file, $upload_dir."/".$target_file)){
+   	echo "File uploaded successfully.";
+   	$mysql_yhteys = mysqli_connect("", "okp", "oli9tRR3", "miikka123");
+	$query = "insert into kuvat (nimi, sijainti, kuvateksti) values (\"$target_file\", \"$upload_dir\", '$kuvateksti');";
+echo $query;
+	
+	$result = mysqli_query($mysql_yhteys, $query)
+        or die("Virhe: " . mysqli_error($mysql_yhteys));
+
+	// mysqli_free_result($result);
+	mysqli_close($mysql_yhteys);  
+
+ } else {
+   echo $_FILES['kuva']['error'];
+ }
 }
 ?>
 </body>
